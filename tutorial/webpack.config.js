8ded0be16dead38f52d5728bf444b2a0f5f52ae2
James Kojima <kors.webworks@gmail.com>
2019-07-23 00:23:13 +0900

    add clean-webpack-plugin

--- tutorial/webpack.config.js
+++ tutorial/webpack.config.js
@@ -3,11 +3,13 @@ const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 // CSS別出しプラグイン
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// webpack distの中身削除プラグイン
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, './dist'),
     // 相対パスの場合
     publicPath: 'dist/'
@@ -50,7 +52,13 @@ module.exports = {
   plugins: [
     new TerserPlugin(),
     new MiniCssExtractPlugin({
-      filename: 'style.css'
+      filename: 'style.[contenthash].css'
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [
+        '**/*',
+        path.join(process.cwd(), 'build/**/*')
+      ]
     })
   ]
 };
